3dbdd94974049ae6665d4798e3be733d6e9a62df
Tássio Montenegro <109485884+tm0nt@users.noreply.github.com>
2024-08-15 10:58:08 -0300

    Depuração para tratar melhor o buffer

--- functions/messageHandler.js
+++ functions/messageHandler.js
@@ -2,54 +2,23 @@ const cleanText = (text) => {
     return text ? text.replace(/@\w+/g, '') : ''; // Remove @username
   };
   
-  const handleMessage = async (bot, msg, groupCapture, groupSender, mediaGroupBuffer) => {
+  const handleMessage = async (bot, msg, groupCapture, groupSender) => {
     try {
       const chatId = msg.chat.id;
-  
+      console.log(msg)
       if (chatId.toString() === groupCapture) {
-        const { caption, photo, video, media_group_id } = msg;
+        const { caption, photo, video } = msg;
   
-        // Verifica se a mensagem faz parte de um grupo de mídia
-        if (media_group_id) {
-          // Inicializa o buffer se ele não existir
-          if (!mediaGroupBuffer[media_group_id]) {
-            mediaGroupBuffer[media_group_id] = [];
-          }
-  
-          // Adiciona a mídia ao buffer correspondente
-          if (photo) {
-            mediaGroupBuffer[media_group_id].push({
-              type: 'photo',
-              media: photo[photo.length - 1].file_id,
-              caption: cleanText(caption),
-            });
-          } else if (video) {
-            mediaGroupBuffer[media_group_id].push({
-              type: 'video',
-              media: video.file_id,
-              caption: cleanText(caption),
-            });
-          }
-  
-          // Verifica se todas as mídias do grupo foram recebidas
-          if (mediaGroupBuffer[media_group_id].length >= photo.length || video) {
-            await bot.sendMediaGroup(groupSender, mediaGroupBuffer[media_group_id]);
-            delete mediaGroupBuffer[media_group_id]; // Limpa o buffer após o envio
-          }
-        } else {
-          // Mensagem fora de grupo de mídia
-          if (photo) {
-            const fileId = photo[photo.length - 1].file_id;
-            await bot.sendPhoto(groupSender, fileId, { caption: cleanText(caption) });
-          } else if (video) {
-            await bot.sendVideo(groupSender, video.file_id, { caption: cleanText(caption) });
-          }
+        if (photo) {
+          const fileId = photo[photo.length - 1].file_id; 
+          await bot.sendPhoto(groupSender, fileId, { caption: cleanText(caption) });
+        } else if (video) {
+          await bot.sendVideo(groupSender, video.file_id, { caption: cleanText(caption) });
         }
       }
     } catch (error) {
-      console.error('Erro ao processar mensagem:', error.message);
+      console.error('Erro ao processar mensagem:', error);
     }
   };
   
   module.exports = { handleMessage };
-  
