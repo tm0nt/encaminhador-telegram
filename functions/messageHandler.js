a3492cfe63ec3929fd175631cb7b0b37b2867c9d
Tássio Montenegro <109485884+tm0nt@users.noreply.github.com>
2024-08-15 10:49:05 -0300

    mensagens agrupadas

--- functions/messageHandler.js
+++ functions/messageHandler.js
@@ -7,10 +7,22 @@ const cleanText = (text) => {
       const chatId = msg.chat.id;
   
       if (chatId.toString() === groupCapture) {
-        const { caption, photo, video } = msg;
+        const { caption, photo, video, media_group_id } = msg;
   
-        if (photo) {
-          const fileId = photo[photo.length - 1].file_id; 
+        // Verifica se é uma foto agrupada
+        if (photo && media_group_id) {
+          const mediaGroup = [];
+          for (const photoItem of msg.media_group) {
+            const fileId = photoItem.file_id;
+            mediaGroup.push({
+              type: 'photo',
+              media: fileId,
+              caption: cleanText(caption),
+            });
+          }
+          await bot.sendMediaGroup(groupSender, mediaGroup);
+        } else if (photo) {
+          const fileId = photo[photo.length - 1].file_id;
           await bot.sendPhoto(groupSender, fileId, { caption: cleanText(caption) });
         } else if (video) {
           await bot.sendVideo(groupSender, video.file_id, { caption: cleanText(caption) });
